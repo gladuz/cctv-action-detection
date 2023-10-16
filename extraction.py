4283587f9b43d175cd6ad92b1b44e23a3850640f
Jamshid Tursunboev <tursunboevjamshid@gmail.com>
2023-10-16 18:28:47 +0900

    Add frame and label extraction code

--- extraction.py
+++ extraction.py
@@ -1,72 +1,96 @@
 #%%
+import argparse
+import os
+import time
+import torch
+from torch.utils.data import Dataset, DataLoader
+from tqdm import trange
 from resnet import resnet50
-import torch
-import torchvision
-import tqdm
-from torchvision.transforms import Compose, Lambda
-from torchvision.transforms._transforms_video import (
-    CenterCropVideo,
-    NormalizeVideo,
-)
-from pytorchvideo.data.encoded_video import EncodedVideo
-from pytorchvideo.transforms import (
-    ApplyTransformToKey,
-    ShortSideScale,
-    UniformTemporalSubsample,
-    UniformCropVideo
-)
-import torch
+from combined_flow_extractor import CombinedFlowModel
+import torchvision.transforms as transforms
+from PIL import Image
+import torch_tensorrt
 
+MEAN=[123.675/255, 116.28/255, 103.53/255]
+STD=[58.395/255, 57.12/255, 57.375/255]
+EVERY_N_FRAMES = 6
+SHORT_SIDE = 512
+class FrameDataset(Dataset):
+
+    def __init__(self, frames_dir) -> None:
+        super().__init__()
+        self.frames_dir = frames_dir
+        self.frame_names = os.listdir(self.frames_dir)
+        self.frame_paths = [os.path.join(self.frames_dir, x) for x in self.frame_names]
+        self.transform = transforms.Compose([
+            transforms.Resize(SHORT_SIDE),
+            transforms.ToTensor(),
+            transforms.Normalize(MEAN, STD),
+        ])
+        self.frame_groups = [[i, i + 2, i+5] for i in range(1, len(self.frame_names)-5, EVERY_N_FRAMES)]
+
+    def __len__(self):
+        return len(self.frame_groups)
+    
+    def __getitem__(self, group_idx):
+        # 000000.jpg 000002.jpg 000005.jpg
+        image_names = [f"{x:06d}.jpg" for x in self.frame_groups[group_idx]]
+        images = [Image.open(os.path.join(self.frames_dir, x)) for x in image_names]
+        images = [self.transform(x) for x in images]
+        images = torch.stack(images)
+        return images
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--device", type=str, default="cuda:0")
+parser.add_argument("--video_id_start", type=int, default=0)
+parser.add_argument("--video_id_end", type=int, default=1908)
+args = parser.parse_args()
+
+videos = os.listdir("/data/common/abb_project/frames")
+video_paths = [os.path.join("/data/common/abb_project/frames", x) for x in videos]
+#dataset = FrameDataset(video_paths[0])
+
+#%%
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+rgb_model = resnet50().to(device)
+flow_model = CombinedFlowModel().to(device)
+rgb_model.eval()
+flow_model.eval()
 
-video_path = 'processed_data/resized_480.mp4'
-side_size = 360
-mean=[123.675, 116.28, 103.53]
-std=[58.395, 57.12, 57.375]
-num_frames = 32
-sampling_rate = 6
-frames_per_second = 30
-alpha = 4
+inputs_rgb = [
+    torch_tensorrt.Input(
+        shape=(1, 3, 512, 768),
+        dtype=torch.float,
+    )
+]
+inputs_flow = [
+    torch_tensorrt.Input(
+        shape=(1, 6, 512, 768),
+        dtype=torch.float,
+    )
+]
+enabled_precisions = {torch.float}  # Run with fp16
 
-#Uniformly select num_frames from the video with a given sampling rate.
-transform =  ApplyTransformToKey(
-    key="video",
-    transform=Compose(
-        [
-            UniformTemporalSubsample(num_frames),
-            NormalizeVideo(mean, std),
-            ShortSideScale(
-                size=side_size
-            ),
-        ]
-    ),
+rgb_module = torch_tensorrt.compile(
+    rgb_model, inputs=inputs_rgb, enabled_precisions=enabled_precisions
 )
+flow_module = torch_tensorrt.compile(
+    flow_model, inputs=inputs_flow, enabled_precisions=enabled_precisions
+)
+torch.jit.save(rgb_module, "checkpoints/resnet_tensorrt.ts")
+torch.jit.save(flow_module, "checkpoints/flownet_tensorrt.ts")
 
-clip_duration = (num_frames * sampling_rate)/frames_per_second
 
-# Code below can be run on loop until the end of the video 
-# by iterating over the start_sec and end_sec variables
-
-start_sec = 0
-end_sec = start_sec + clip_duration
-
-# Initialize an EncodedVideo helper class
-video = EncodedVideo.from_path(video_path)
-
-# Load the desired clip
-video_data = video.get_clip(start_sec=start_sec, end_sec=end_sec)
-
-# Apply a transform to normalize the video input
-video_data = transform(video_data)
-
-# Move the inputs to the desired device
-inputs = video_data["video"]
-inputs = inputs.transpose(0,1).to(device)
 
 # %%
-model = resnet50()
-model.to(device)
-model.eval()
-with torch.no_grad():
-    outputs = model(inputs)
-print(outputs.size())
+for i in trange(args.video_id_start, args.video_id_end):
+    dataset = FrameDataset(video_paths[i])
+    dataloader = DataLoader(dataset, batch_size=128, shuffle=False, num_workers=8)
+    for batch in dataloader:
+        batch = batch.to(device)
+        st_time = time.time()
+        with torch.no_grad():
+            rgb_features = rgb_module(batch[:, 1, :, :, :]).squeeze()
+            flow_features = flow_module(torch.cat([batch[:, 0, :, :, :], batch[:, 2, :, :, :]], dim=1)).squeeze()
+        print(time.time() - st_time)
+# %%
--- frame_extractor.py
+++ frame_extractor.py
@@ -6,7 +6,11 @@ from tqdm import tqdm
 DATA_PATH = '/data/common/abb_project/resized'
 SAVE_PATH = '/data/common/abb_project/frames'
 
-videos = glob.glob(os.path.join(DATA_PATH, '**', '*.mp4'), recursive=True)
+#videos = glob.glob(os.path.join(DATA_PATH, '**', '*.mp4'), recursive=True)
+videos = open('processed_data/explore_error_files.txt', 'r').read().split('\n')
+videos = [videos.replace(".xml", "_resized.mp4") for videos in videos]
+videos = [videos.replace("xmls", "resized") for videos in videos]
+print(videos)
 error_files = set()
 with open('processed_data/flow_error_files.txt', 'r') as f:
     for line in f:
@@ -15,8 +19,9 @@ for video_file in tqdm(videos, desc='Extracting videos'):
     frames_folder_name = "__".join(video_file.split('/')[5:]).replace('.mp4', '')
     if video_file in error_files:
         continue
+    #os.removedirs(os.path.join(SAVE_PATH, frames_folder_name))
     if not os.path.exists(os.path.join(SAVE_PATH, frames_folder_name)):
         os.makedirs(os.path.join(SAVE_PATH, frames_folder_name))
         # extract frames with ffmpeg 30 fps quality 2 for jpg
-        command = 'ffmpeg -i "{}" -vf fps=30 "{}/%06d.jpg"'.format(video_file, os.path.join(SAVE_PATH, frames_folder_name))
-        os.system(command)
+    command = 'ffmpeg -i "{}" -vf fps=30 "{}/%06d.jpg"'.format(video_file, os.path.join(SAVE_PATH, frames_folder_name))
+    os.system(command)
